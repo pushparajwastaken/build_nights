bfd609c00dd5c44a4faa8a79dd2e7d00c0d5ffc5
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-12-08 23:34:28 +0530

    type narrowing

---
+++ typescript/src/typenarrowing.ts
@@ -0,0 +1,67 @@
+function getChai(kind: string | number) {
+  if (typeof kind === "string") {
+    return `Making ${kind} Chai...`;
+  }
+  return `Chai order:${kind}`;
+}
+console.log(getChai(6));
+
+function serveChai(msg?: string) {
+  if (msg) {
+    return `${msg}`;
+  }
+  return "Default Message";
+}
+
+function orderChai(size: "medium" | "small" | "cutting" | number) {
+  if (size === "small") {
+    return `Serving ${size}`;
+  }
+  if (size === "medium" || "cutting") {
+    return `Serving extra ${size}`;
+  }
+  return `Serving ${size} cups of Chai`;
+}
+
+class KulhadChai {
+  serve() {
+    return `Serving Kulhad Chai`;
+  }
+}
+
+class CuttingChai {
+  serve() {
+    return `Serving Cutting Chai`;
+  }
+}
+
+function serve(chai: KulhadChai | CuttingChai) {
+  if (chai instanceof KulhadChai) {
+    return chai.serve();
+  }
+}
+type ChaiOrder = {
+  type: string;
+  sugar: number;
+};
+
+function isChaiOrder(obj: any): obj is ChaiOrder {
+  return (
+    typeof obj === "object" &&
+    obj != null &&
+    typeof obj.type === "string" &&
+    typeof obj.sugar === "number"
+  );
+}
+
+function serveOrder(item: ChaiOrder | string) {
+  if (isChaiOrder(item)) {
+    return `Serving ${item.sugar}`;
+  }
+  return `Serving Custom Chai ${item}`;
+}
+type MasalaChai = { type: "Masala"; spicelevel: number };
+
+type Chai = "string" | MasalaChai;
+
+function isStringArray(arr: unknown): arr is string[] {}
--- typescript/src/union.ts
+++ typescript/src/union.ts
@@ -2,6 +2,7 @@ let chai: string | number = "3 cups";
 console.log(chai);
 let cups = ["11", "22", "32", "34"];
 let orderedCups: string | undefined;
+let typeofChai: "Masala" | "Sugarless" | "Cutting";
 for (let cup of cups) {
   if (cup == "22") {
     orderedCups = cup;
