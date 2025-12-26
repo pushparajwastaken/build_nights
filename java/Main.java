201ec887644e892cee27c5e11375c6c4d20dbf71
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-12-26 22:14:16 +0530

    add java

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+    System.out.println("Hello! How are you?");
+    Scanner input=new Scanner(System.in);
+    
+    System.out.print(input.nextInt());
+    }
+}
